416d6c331c3d9812c6890f3b148e7894fbdad366
Junaid Rasheed <jrasheed178@gmail.com>
2020-01-22 15:19:40 +0000

    Update heron specification
    + Prevent integer overflow by changing 2^2^N  to use real exponents
    + Extract relevant VCs from generated why3 file
    + Translate human friendly goals from generated VCs

--- spark/heron.adb
+++ spark/heron.adb
@@ -3,7 +3,7 @@ with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 
 package body Heron with SPARK_Mode is
 
-   function Heron (X : Float; N : Positive) return Float is
+   function Heron (X : Float; N : Integer) return Float is
       Y : Float := 1.0;
    begin
       
@@ -12,9 +12,8 @@ package body Heron with SPARK_Mode is
          pragma Loop_Invariant (Y >= 0.5);
          pragma Loop_Invariant (i <= N);
          pragma Loop_Invariant 
-           (Real_Abs 
-              (Real_Square_Root (Real_From_Float(X)) - Real_From_Float(Y)) 
-            <= Real_From_Float((1.0 + Eps) ** (3 * i)) / Real_From_Int(2 ** (2 ** i)));
+           (Rabs (Real_Square_Root (Rf(X)) - Rf(Y)) 
+            <= Real_Pow((Ri(1) + Rf(Eps)), Ri(3 * i)) / Real_Pow(Ri(2), Real_Pow(Ri(2), Ri(i))));
 
          
          Y := (Y + X/Y) / 2.0;
--- spark/heron.ads
+++ spark/heron.ads
@@ -3,10 +3,10 @@ with Reals; use Reals;
 package Heron with SPARK_Mode is
    Eps : Float := 2.0 ** (-23);
    
-   function Heron (X : Float; N : Positive) Return Float with
-     Pre => (X >= 0.5 and X <= 2.0) and N <= 5 and Eps = 2.0 ** (-23),
+   function Heron (X : Float; N : Integer) Return Float with
+     Pre => (X >= 0.5 and X <= 2.0) and N > 0 and N <= 5 and Eps = 2.0 ** (-23),
      Post => 
-       Real_Abs (Real_Square_Root (Real_From_Float(X)) - Real_From_Float(Heron'Result))
-         <= Real_From_Float((1.0 + Eps) ** (2 * N)) / Real_From_Int(2 ** (2 ** N));
+       Rabs (Real_Square_Root (Rf(X)) - Rf(Heron'Result))
+         <= Real_Pow((Ri(1) + Rf(Eps)), Ri(3 * N)) / Real_Pow(Ri(2), Real_Pow(Ri(2), Ri(N)));
 
 end Heron;
