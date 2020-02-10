fc1f1c45caa0113ad3181848e7b49a91697ad325
Junaid Rasheed <jrasheed178@gmail.com>
2020-02-10 22:33:14 +0000

    coq: (mostly) finish heron_presrvation_T proof
    + Final thing, need to prove something similar to
        + 6 * INR (i - 1) + 6 = 6 * INR i
        + Without INR, this can be done by lra

--- coq/heron_preservation.v
+++ coq/heron_preservation.v
@@ -1,4 +1,5 @@
 Require Import Reals.
+Require Import Lra.
 Require Import Interval.Interval_tactic.
 
 From mathcomp Require Import all_ssreflect.
@@ -11,23 +12,21 @@ Open Scope R_scope.
 Definition Eps := (1/2)^23.
 Definition onePe := (1 + Eps).
 Definition oneMe := (1 - Eps).
+Definition RU x := (x) * onePe.
+Definition RD x := (x) * oneMe.
 
 Lemma heron_preservation_TRM x y:
-  let RU x := (x) * onePe in
-  let RD x := (x) * oneMe in
   let y'R := RD(RD(y + RD(x/y))/2) in
   let y'E := (y + (x/y))/2 in
-  (sqrt x - y'R) = (sqrt x - y'E) - (y'R - y'E) ->
-  Rabs (sqrt x - y'E) - Rabs (y'R - y'E) <= Rabs (sqrt x - y'R).
+  Rabs (sqrt x - y'R) <= Rabs (sqrt x - y'E) + Rabs (y'E - y'R).
 Proof. 
   intros.
-  rewrite H.
-  apply Rabs_triang_inv.
+  have->: (sqrt x - y'R) = (sqrt x - y'E) + (y'E - y'R)
+    by lra.
+  apply Rabs_triang.
 Qed.
 
 Lemma heron_preservation_R x y:
-  let RU x := (x) * onePe in
-  let RD x := (x) * oneMe in
   let y'R := RD(RD(y + RD(x/y))/2) in
   let y'E := (y + (x/y))/2 in
   0.5 <= x <= 2.0 ->
@@ -40,8 +39,6 @@ Proof.
 Admitted.
 
 Lemma heron_preservation_M x y (N : nat) (i : nat):
-  let RU x := (x) * onePe in
-  let RD x := (x) * oneMe in
   let y'R := RD(RD(y + RD(x/y))/2) in
   let y'E := (y + (x/y))/2 in
   0.5 <= x <= 2.0 ->
@@ -56,9 +53,14 @@ Proof.
   intros.
 Admitted.
 
+(* Without INR, this can be proved easily with lra *)
+Lemma temp (i :nat): 
+ (1 / 2 * (1 / 2 * 1)) ^ i + 6 * INR (i - 1) * Eps + 6 * Eps = 
+ (1 / 2 * (1 / 2 * 1)) ^ i + 6 * INR i * Eps.
+Proof.
+Admitted.
+
 Lemma heron_preservation_T x y (N : nat) (i : nat):
-  let RU x := (x) * onePe in
-  let RD x := (x) * oneMe in
   let y'R := RD(RD(y + RD(x/y))/2) in
   let y'E := (y + (x/y))/2 in
   0.5 <= x <= 2.0 ->
@@ -71,8 +73,15 @@ Lemma heron_preservation_T x y (N : nat) (i : nat):
   Rabs(sqrt(x) - y'R) <= (((1/2)^2)^i) + (6 * INR(i) * Eps).
 Proof.
   intros.
-  destruct (heron_preservation_R H H0).
-  apply H1. apply H2.
-  destruct (heron_preservation_M H H0 H1 H2 H3 H4 H5).
-  apply H6.
-Admitted. 
+  have TRM //:= heron_preservation_TRM x y. fold y'R y'E in TRM. simpl in TRM.
+  have R //:= heron_preservation_R H H0 H1 H2. fold y'R y'E in R.
+  have M //:= (heron_preservation_M H H0 H1 H2 H3 H4 H5 H6). fold y'R y'E in M. simpl in M.
+  have T: Rabs (sqrt x - y'E) + Rabs (y'E - y'R) <= (1 / 2 * (1 / 2 * 1)) ^ i + 6 * INR (i - 1) * Eps + 6 * Eps.
+  by lra.
+  simpl.
+  apply (@Rle_trans (Rabs (sqrt x - y'R)) (Rabs (sqrt x - y'E) + Rabs (y'E - y'R)) ((1 / 2 * (1 / 2 * 1)) ^ i + 6 * INR i * Eps)).
+  apply TRM.
+  rewrite (temp i) in T.
+  apply T.
+Qed.
+ 
