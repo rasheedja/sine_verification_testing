a08291cb9c7b02b60a3191c30a5da80845939cf2
Junaid Rasheed <jrasheed178@gmail.com>
2021-07-27 15:36:34 +0100

    Verify Approx_Sin, Approx_Cos

--- spark/libsin2.adb
+++ spark/libsin2.adb
@@ -21,7 +21,7 @@ package body libsin2 with SPARK_Mode is
    function My_Copy_Sign (X : Float; S : Float) return Float is
    begin
       if X = 0.0 then
-         return X;
+        return X;
       else 
          if S > 0.0 then
             return X;
@@ -53,102 +53,88 @@ package body libsin2 with SPARK_Mode is
       C3     : constant Float := 0.000000000992088189377682284;
       C4     : constant Float := K - C1 - C2 - C3;
       N      : constant Float := My_Machine_Rounding (X * K**(-1));
-
    begin
-      if abs X < Max_X then
-
-         --  X is in the range for which strict accuracy can be provided
-
-         X := (((X - N * C1) - N * C2) - N * C3) - N * C4;
-         Q := Integer (N) mod 4; -- Return Integer (N) for specification
-         R := Integer (N);
-
-      elsif not X'Valid then
-
-         --  X is an infinity or NaN
-
-         X := X - X;
-         Q := 0;
-         R := 0;
-
-      else
-         --  X := 0.1234;
-         --  Q := 0;
-         --  R := 1234;
-         Reduce_Half_Pi_Large (X, N, Q, R);
-      end if;
+      X := (((X - N * C1) - N * C2) - N * C3) - N * C4;
+      Q := Integer (N) mod 4; -- Return Integer (N) for specification
+      R := Integer (N);
    end Reduce_Half_Pi;
    
-   procedure Reduce_Half_Pi_Large (X : in out Float; N : Float; Q : out Quadrant; R : out Integer) is
-      type Int_64 is range -2**63 .. 2**63 - 1; -- used for conversions
-
-      HM : constant Positive := Float'Machine_Mantissa / 2;
-      C1 : constant Float := 1.5703125;
-      C2 : constant Float := 0.000483751296997070313;
-      C3 : constant Float := 0.0000000754953362047672272;
-      C4 : constant Float := Half_Pi - C1 - C2 - C3;
-
-      K    : Float := N;
-      K_Hi : Float;
-      K_Lo : Float;
-
-   begin
-      Q := 0;
-      R := 0;
-      
-      loop
-         Split_Veltkamp (X => K, X_Hi => K_Hi, X_Lo => K_Lo);
-
-         X := Multiply_Add (-K_Hi, C1, X);
-         X := Multiply_Add (-K_Hi, C2, Multiply_Add (-K_Lo, C1, X));
-         X := Multiply_Add (-K_Hi, C3, Multiply_Add (-K_Lo, C2, X));
-         X := Multiply_Add (-K_Hi, C4, Multiply_Add (-K_Lo, C3, X));
-         X := Multiply_Add (-K_Lo, C4, X);
-
-         if abs K < 2.0**62 or else abs K_Lo <= 2.0**62 then
-            Q := Quadrant ((Int_64 (Q) + Int_64 (N)) mod 4);
-            R := Integer (Int_64 (Q) + Int_64 (N));
-         end if;
-
-         exit when X in -0.26 * Pi .. 0.26 * Pi;
-
-         K := My_Machine_Rounding (X * Half_Pi**(-1));
-      end loop;
-   end Reduce_Half_Pi_Large;
+   --  procedure Reduce_Half_Pi_Large (X : in out Float; N : Float; Q : out Quadrant; R : out Integer) is
+   --     type Int_64 is range -2**63 .. 2**63 - 1; -- used for conversions
+   --  
+   --     HM : constant Positive := Float'Machine_Mantissa / 2;
+   --     C1 : constant Float := 1.5703125;
+   --     C2 : constant Float := 0.000483751296997070313;
+   --     C3 : constant Float := 0.0000000754953362047672272;
+   --     C4 : constant Float := Half_Pi - C1 - C2 - C3;
+   --  
+   --     K    : Float := N;
+   --     K_Hi : Float;
+   --     K_Lo : Float;
+   --  
+   --  begin
+   --     Q := 0;
+   --     R := 0;
+   --  
+   --     loop
+   --        Split_Veltkamp (X => K, X_Hi => K_Hi, X_Lo => K_Lo);
+   --  
+   --        X := Multiply_Add (-K_Hi, C1, X);
+   --        X := Multiply_Add (-K_Hi, C2, Multiply_Add (-K_Lo, C1, X));
+   --        X := Multiply_Add (-K_Hi, C3, Multiply_Add (-K_Lo, C2, X));
+   --        X := Multiply_Add (-K_Hi, C4, Multiply_Add (-K_Lo, C3, X));
+   --        X := Multiply_Add (-K_Lo, C4, X);
+   --  
+   --        if abs K < 2.0**62 or else abs K_Lo <= 2.0**62 then
+   --           Q := Quadrant ((Int_64 (Q) + Int_64 (N)) mod 4);
+   --           R := Integer (Int_64 (Q) + Int_64 (N));
+   --        end if;
+   --  
+   --        exit when X in -0.26 * Pi .. 0.26 * Pi;
+   --  
+   --        K := My_Machine_Rounding (X * Half_Pi**(-1));
+   --     end loop;
+   --  end Reduce_Half_Pi_Large;
    
-   function Compute_Horner (P : Polynomial; X : Float) return Float is
-      Result : Float := P (P'Last);
+   --  function Compute_Horner (P : Polynomial; X : Float) return Float is
+   --     Result : Float := P (P'Last);
+   --  
+   --  begin
+   --     for P_j of reverse P (P'First .. P'Last - 1) loop
+   --        Result := Multiply_Add (Result, X, P_j);
+   --     end loop;
+   --  
+   --     return Result;
+   --  end Compute_Horner;
 
-   begin
-      for P_j of reverse P (P'First .. P'Last - 1) loop
-         Result := Multiply_Add (Result, X, P_j);
-      end loop;
-
-      return Result;
-   end Compute_Horner;
-
+   function Exact (X : Long_Float) return Float is (Float (X));
+   
    function Approx_Sin  (X : Float) return Float is
       --  Note: The reference tables named below for sine lists constants
       --  for sin((pi/4) * x) ~= x * P (x^2), in order to get sin (x),
       --  the constants have been adjusted by division of appropriate
       --  powers of (pi/4) ^ n, for n 1,3,5, etc.
-      Sin_P  : constant Polynomial :=
-           (1 => Exact (-0.16666_65022),
-            2 => Exact (0.83320_16396E-2),
-            3 => Exact (-0.19501_81843E-3));
+      --  Sin_P  : constant Polynomial :=
+      --       (1 => Float(Long_Float (-0.16666_65022)),
+      --        2 => Float(Long_Float (0.83320_16396E-2)),
+      --        3 => Float(Long_Float (-0.19501_81843E-3)));
 
       Sqrt_Epsilon_LF : constant Long_Float :=
         Sqrt_2 ** (1 - Long_Float'Machine_Mantissa); -- substitute Long_Long_Float'Machine_Mantissa 
                                                           -- Get rid of exacts, long_long_floats
 
       G  : constant Float := X * X;
-
+      
+      -- Compute_Horner Manually
+      H0 : constant Float := (-0.19501_81843E-3);
+      H1 : constant Float := (H0 * G + (0.83320_16396E-2));
+      H2 : constant Float := (H1 * G + (-0.16666_65022));
    begin
-      if abs X <= Exact (Sqrt_Epsilon_LF) then
+      if abs X <= Float(Long_Float (Sqrt_Epsilon_LF)) then
          return X;
       end if;
 
-      return Multiply_Add (X, Compute_Horner (Sin_P, G) * G, X);
+      return (X * (H2 * G) + X);
    end Approx_Sin;
 
    function Sin (X : Float) return Float is
@@ -172,14 +158,22 @@ package body libsin2 with SPARK_Mode is
       --  constants for cos((pi/4) * x) ~= P (x^2), in order to get
       --  cos (x), the constants have been adjusted by division of
       --  appropriate  powers of (pi/4) ^ n, for n 0,2,4,6 etc.
-      Cos_P : constant Polynomial :=
-        (0 => Exact (0.99999_99999),
-         1 => Exact (-0.49999_99957),
-         2 => Exact (0.41666_61323E-1),
-         3 => Exact (-0.13888_52915E-2),
-         4 => Exact (0.24372_67909E-4));
+      --  Cos_P : constant Polynomial :=
+      --    (0 => (0.99999_99999),
+      --     1 => (-0.49999_99957),
+      --     2 => (0.41666_61323E-1),
+      --     3 => (-0.13888_52915E-2),
+      --     4 => (0.24372_67909E-4));
+      
+      G  : constant Float := X * X;
+      
+      H0 : constant Float := (0.24372_67909E-4);	
+      H1 : constant Float := (H0 * G + (-0.13888_52915E-2));
+      H2 : constant Float := (H1 * G + (0.41666_61323E-1));
+      H3 : constant Float := (H2 * G + (-0.49999_99957));
+      H4 : constant Float := (H3 * G + (0.99999_99999));
    begin
-      return Compute_Horner (Cos_P, X * X);
+      return H4;
    end Approx_Cos;
 
 end libsin2;
--- spark/libsin2.ads
+++ spark/libsin2.ads
@@ -1,7 +1,12 @@
 with Ada.Numerics; use Ada.Numerics;
+with Reals; use Reals;
 
 package libsin2 with SPARK_Mode is
    subtype Quadrant is Integer range 0 .. 3;
+   
+   function Multiply_Add (X, Y, Z : Float) return Float with
+     Pre => (-100000.0 <= X and X <= 100000.0 and -100000.0 <= Y and Y <= 100000.0 and -100000.0 <= Z and Z <= 100000.0),
+     Post => Multiply_Add'Result = X * Y + Z;
    
    Max_Red_Trig_Arg : constant := 0.26 * Ada.Numerics.Pi;
    Half_Pi          : constant := Ada.Numerics.Pi / 2.0;
@@ -9,35 +14,51 @@ package libsin2 with SPARK_Mode is
 
    type Polynomial is array (Natural range <>) of Float;
 
-   function My_Machine_Rounding (X : Float) return Float;
+   function My_Machine_Rounding (X : Float) return Float with
+     Post => 
+       (if X - Float'Floor(X) < 0.5 
+              then My_Machine_Rounding'Result = Float'Floor(X) 
+              else My_Machine_Rounding'Result = Float'Ceiling(X)) 
+       and (X - My_Machine_Rounding'Result) <= 0.5 
+     and (X - My_Machine_Rounding'Result) >= -0.5;
    
    procedure Split_Veltkamp (X : Float; X_Hi, X_Lo : out Float)
-     with Post => X = X_Hi + X_Lo;
+     with 
+       Pre => (-100000.0 <= X and X <= 100000.0),
+       Post => X = X_Hi + X_Lo;
 
    procedure Reduce_Half_Pi (X : in out Float; Q : out Quadrant; R : out Integer) -- Might need to return number of Pi reductions here (for specification)
-     with Pre  => X >= 0.0,
-     Post => abs (X - (X'Old - (Float(R) * Half_Pi))) <= 0.0001;
-   procedure Reduce_Half_Pi_Large (X : in out Float;  N : Float; Q : out Quadrant; R : out Integer);
-   
-   function Compute_Horner (P : Polynomial; X : Float) return Float with Inline;
+     with Pre  => X >= 0.0 and X <= 500.0,
+     Post => abs(X) <= Max_Red_Trig_Arg and abs(X - (X'Old - (Float(R) * (Ada.Numerics.Pi / 2.0)))) <= 0.01;
+   --  procedure Reduce_Half_Pi_Large (X : in out Float;  N : Float; Q : out Quadrant; R : out Integer)
+   --    with Pre  => X >= 0.0 and X <= 100000.0,
+   --    Post => abs (X - (X'Old - (Float(R) * Half_Pi))) <= 0.0001;
+   --  function Compute_Horner (P : Polynomial; X : Float) return Float with
+   --    Pre => (P'First >= 0 and P'First <= 1 and P'Last >= 3 and P'Last <= 4 and -1000.0 <= X and X <= 1000.0 and (for all I in P'Range => abs(P(I)) <= 1.0));
 
    function Maximum_Relative_Error (X : Float) return Float is (0.0 * X);
    
-   function Exact (X : Long_Float) return Float is (Float (X));
+   function Exact (X : Long_Float) return Float
+     with
+       Pre => abs(X) <= 1000.0,
+       Post => Exact'Result = Float(X);
    
    function Epsilon return Float is (Float'Model_Epsilon);
    
    function Approx_Sin  (X : Float) return Float
-     with Pre  => (abs X) <= Exact (Max_Red_Trig_Arg),
-     Post => abs Approx_Sin'Result <= Exact (1.0) and then
-     Maximum_Relative_Error (Approx_Sin'Result) <= 2.0 * Epsilon;
+     with Pre  => (abs X) <= (Max_Red_Trig_Arg),
+     Post => abs Approx_Sin'Result <= (1.0) and then
+     Maximum_Relative_Error (Approx_Sin'Result) <= 2.0 * Epsilon and then
+     Rabs (Rf(Approx_Sin'Result) - Real_Sin(Rf(X))) <= Rf(30.0/8388608.0);
 
    function Approx_Cos (X : Float) return Float
-     with Pre  => abs (X) <= Exact (Max_Red_Trig_Arg),
-     Post => abs (Approx_Cos'Result) <= Exact (1.0)
+     with Pre  => abs (X) <= (Max_Red_Trig_Arg),
+     Post => abs (Approx_Cos'Result) <= (1.0)
      and then Maximum_Relative_Error (Approx_Cos'Result)
-     <= 2.0 * Epsilon;
+     <= 2.0 * Epsilon and then
+     Rabs (Rf(Approx_Cos'Result) - Real_Cos(Rf(X))) <= Rf(30.0/8388608.0);
 
    function Sin (X : Float) return Float
-     with Export, Convention => C, External_Name => "sinf";
+     with Pre => abs (X) <= 500.0, Export, Convention => C, External_Name => "sinf",
+     Post => Rabs (Rf(Sin'Result) - Real_Sin(Rf(X))) <= Rf(0.01);
 end libsin2;
